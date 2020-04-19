c9d8e2ba59e2b5439babc49e4f7f7df4c5d1ea3e
ghadeerda <ghadeerdarwesh32@gmail.com>
2020-04-19 22:43:36 +0500

    Basic setup

--- app/routes.py
+++ app/routes.py
@@ -4,11 +4,14 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from app import app
 from db import get_db
 
+
+#for testing
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
-    
+
+#error page    
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('error.html'), 404
