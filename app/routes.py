739c8ed25fdba15811542b9b2cf6e0dfd5bd5464
ghadeerda <ghadeerdarwesh58@gmail.com>
2020-04-19 22:32:39 +0500

    Basic setup

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template,request,send_from_directory,session,flash
+import re
+from werkzeug.security import check_password_hash, generate_password_hash
+from app import app
+from db import get_db
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
