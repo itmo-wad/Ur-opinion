fee62e1a6a38ead805f2b1b529d9a36ce93df2e5
ghadeerda <ghadeerdarwesh32@gmail.com>
2020-04-25 00:48:13 +0500

    register and login functions

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,send_from_directory,session,flash
+from flask import Flask, render_template,request,send_from_directory,session,flash,redirect
 import re
 from db import *
 
@@ -6,18 +6,39 @@ from db import *
         
 
 def index_r():
-    return render_template('index.html') 
+    #check if the user logged in, if not go back to login html
+    if not session.get('logged_in'):
+          return redirect("/login", code=302)
+          
+    else:
+        return render_template('index.html') 
 
 #login page
-def log_r(username,password,email,fullname):
-
+def reg_r(username,password,email,fullname):
+    
+    #check if user already exsits
+    if check_user_in_db(username) :
+                     flash('Username aleady exists!')
+                     session['logged_in'] = False
+                     return reg_r()
+    else:
         add_user_to_db(username, password,email,fullname)
-        session['logged_in'] = True
+        session['logged_in'] = False
         return index_r()
     
   #  return render_template('login.html')
 
 #register page
-def reg_r():
-    return render_template('register.html')  
+def log_r(username,password):
+    if check_user_in_db(username):
+            if check_pass_in_db(username, password):
+                session['logged_in'] = True
+            else :
+                flash('Wrong Password!')
+                session['logged_in'] = False
+    else:
+            flash('User not exsit!!')
+            session['logged_in'] = False
+                    
+    return redirect("/")
 
