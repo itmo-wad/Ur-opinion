76986fecbb6fe4d19f52be2829f2b3e0b2fd23e0
ghadeerda <ghadeerdarwesh32@gmail.com>
2020-04-29 13:17:41 +0500

    update js files

--- static/js/script.js
+++ static/js/script.js
@@ -20,10 +20,9 @@ window.onclick = function (event) {
 
 /* show adn hid div for new task and account setting
  */
-function show_perview(name, title, text) {
+function show_new_task(name, title, text) {
 
     var dev_block = document.getElementById("preview");
-
     dev_block.style.display = "block";
 
 
@@ -31,7 +30,24 @@ function show_perview(name, title, text) {
 };
 
 
-function hide_perview() {
+function hide_new_task() {
     var test = document.getElementById("preview");
+    test.style.display = "none";
+};
+
+
+
+function show_new_team(name, title, text) {
+
+    var dev_block = document.getElementById("preview_team");
+    dev_block.style.display = "block";
+
+
+
+};
+
+
+function hide_new_team() {
+    var test = document.getElementById("preview_team");
     test.style.display = "none";
 };
