fbeec94fb1d1c3616127988982147139db1d86e2
ghadeerda <ghadeerdarwesh32@gmail.com>
2020-05-02 11:17:23 +0500

    create teams list

--- db.py
+++ db.py
@@ -8,10 +8,9 @@ import pymongo
 
 #client = pymongo.MongoClient("mongodb://<dbuser>:<password>@ds141952.mlab.com:41952/heroku_kmd3257w?retryWrites=false&w=majority")
 #db = client["dbname"]
-#client = pymongo.MongoClient(os.environ.get('MongoDb', None))
-#db = client.get_default_database()
-client = pymongo.MongoClient("mongodb://admin:P29069921@ds141952.mlab.com:41952/heroku_kmd3257w?retryWrites=false&w=majority")
+client = pymongo.MongoClient(os.environ.get('MongoDb', None))
 db = client.get_default_database()
+
 
 #get users' collection
 users = db["users"]
