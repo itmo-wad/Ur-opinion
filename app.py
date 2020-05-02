967e583082b25e43b078e8f122c6d4176a3cdcd0
ghadeerda <ghadeerdarwesh32@gmail.com>
2020-05-03 02:00:32 +0500

    handle 405 error

--- app.py
+++ app.py
@@ -158,6 +158,10 @@ def logout():
 def page_not_found(e):
     return render_template('error.html'), 404
 
+#method not allowed    
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return render_template('error.html'), 405
  
 @app.errorhandler(500)
 def internal_error(e):
